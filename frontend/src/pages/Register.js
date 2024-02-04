606bc69a451b56b9f81fb46ade2265aee2069302
Nawel ALIOUALI <130800060+Nallywell@users.noreply.github.com>
2024-02-04 21:44:52 +0100

    Login & Register

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import backgroundImage from "../img/Background.PNG";
+
 const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -14,7 +16,10 @@ const Register = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+
+ 
+    
       <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-3xl w-50 max-w-md">
         <div className="font-medium self-center text-xl sm:text-3xl text-gray-800">
           Register Now
